dab56de431e01b0c93699fd9e859ccd6f59e67a3
Victorgabrielcruz <112908892+Victorgabrielcruz@users.noreply.github.com>
2023-09-01 18:15:20 -0300

    att15

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
-dotenv.config();
-
 import roteadorLogin from "./routes/login.js";
 import roteadorUsuario from "./routes/usuario.js";
+dotenv.config();
+
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(roteadorUsuario);
 app.use(roteadorLogin);
-app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.json({
     message: "API para CRUD usuario: https://github.com/Victorgabrielcruz21",
